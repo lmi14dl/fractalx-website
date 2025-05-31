e5cf63d5a12d421da25c94f00fe8d69312ff62bc
Milad <arsenfalken@gmail.com>
2025-05-31 13:10:03 +0330

    Update settings.py

--- fractal/fractal/settings.py
+++ fractal/fractal/settings.py
@@ -161,7 +161,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/fractalx/public_html/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
