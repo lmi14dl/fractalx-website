4138f00a6a68d9f09aac4beddf7cef2bc560f6f5
Milad <arsenfalken@gmail.com>
2025-05-31 13:21:23 +0330

    Update settings.py

--- fractal/fractal/settings.py
+++ fractal/fractal/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = '/home/fractalx/public_html/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
