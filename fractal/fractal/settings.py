a161e7b5fd6918e2aca864474d0394537eff5c17
Milad <arsenfalken@gmail.com>
2025-05-31 12:23:39 +0330

    Update settings.py

--- fractal/fractal/settings.py
+++ fractal/fractal/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+pymysql.install_as_MySQLdb()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -100,10 +101,21 @@ WSGI_APPLICATION = 'fractal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fractalx_db',
+        'USER': 'fractalx_db',
+        'PASSWORD': 'fractal-db',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
