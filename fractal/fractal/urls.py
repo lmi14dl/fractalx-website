fcdd50ae551729b2659fe26642eba609e0782a6a
Milad <arsenfalken@gmail.com>
2025-06-04 09:16:23 +0330

    Update urls.py

--- fractal/fractal/urls.py
+++ fractal/fractal/urls.py
@@ -15,17 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns 
+from django.views.static import serve
 
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),  
     path('accounts/', include('allauth.urls')), 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += i18n_patterns(
     path('', include('home.urls')),
